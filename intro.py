7746d04ed2fd69925673321afbaf6fd0621b13be
Priyanka <priyankab813@gmail.com>
2026-01-23 00:07:55 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,12 @@
+# Prints "Hello, Machine Learning!"
+# Stores your name in a variable and prints: "Hello, [Your Name]!"
+# Asks for user input and prints: "Welcome to ML, [User's Name]!"
+# Asks for the user's age and calculates their age after 5 years
+
+print("Hello, Machine Learning!")
+var=input("Enter your name:")
+print("Hello,",var)
+print("Welcome to ML,",var+"!")
+age=int(input("Enter your age:"))
+print("Your age after five year will be:"  ,age +5)
+
